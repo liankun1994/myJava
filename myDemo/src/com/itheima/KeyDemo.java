a44da8d29397fe2d88d13b2d59dce6f43aec3f0a
liankun <liankun1994@live.com>
2018-09-07 12:19:56 +0800

    练习快捷键。

---
+++ myDemo/src/com/itheima/KeyDemo.java
@@ -0,0 +1,20 @@
+package com.itheima;
+
+public class KeyDemo {
+	public static void main(String[] args) {
+		System.out.println("main syso + alt + /  okokok");
+
+//		注释 carl + /
+//		多行 alt shift + / 取消 alt + shift + \
+//		格式化 ctrl + shift + f
+		/*
+		 * System.out.println("main syso + alt + /  okokok");
+		 * System.out.println("main syso + alt + /  okokok");
+		 * System.out.println("main syso + alt + /  okokok");
+		 * System.out.println("main syso + alt + /  okokok");
+		 * System.out.println("main syso + alt + /  okokok");
+		 */
+
+	}
+
+}
