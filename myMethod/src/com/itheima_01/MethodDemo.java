f5b2de0e96497a5b741e2324d901a42a22145dff
liankun <liankun1994@live.com>
2018-09-12 22:53:27 +0800

    方法。

---
+++ myMethod/src/com/itheima_01/MethodDemo.java
@@ -0,0 +1,14 @@
+package com.itheima_01;
+
+public class MethodDemo {
+	public static void main(String[] args) {
+		int result = sum(1,2);
+		System.out.println(result);
+	}
+	
+	public static int sum(int a,int b) {
+		int c;
+		c = a + b;
+		return c;
+	}
+}
