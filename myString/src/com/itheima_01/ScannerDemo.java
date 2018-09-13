bd42fefa58fc556426241c8dd916f978383c289e
zlk_xx <liankun@live.com>
2018-09-13 20:41:27 +0800

    API的使用。

---
+++ myString/src/com/itheima_01/ScannerDemo.java
@@ -0,0 +1,14 @@
+package com.itheima_01;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("ÇëÊäÈëÒ»¸ö×Ö·û´®£º");
+		String s = sc.nextLine();
+		
+		System.out.println(s);
+	}
+}
